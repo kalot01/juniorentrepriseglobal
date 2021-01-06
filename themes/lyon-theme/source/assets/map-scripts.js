3df6583b2e3e60cf77a5e56e9aed32f98ab50ad4
kalot01 <59099503+kalot01@users.noreply.github.com>
2021-01-06 14:28:17 +0100

    Update map-scripts.js

--- themes/lyon-theme/source/assets/map-scripts.js
+++ themes/lyon-theme/source/assets/map-scripts.js
@@ -169,7 +169,7 @@ function fetchdata() {
       mapdb = JSON.parse(xhttp.responseText);
       elements = mapdb.map((el) => el.id);
       for (let i of elements) {
-        if (i >= 60) {
+        if (i >= 122) {
           let xhttpS = new XMLHttpRequest();
           xhttpS.onreadystatechange = function () {
             if (xhttpS.responseText != "") {
